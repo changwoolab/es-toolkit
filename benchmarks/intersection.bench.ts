23316ae1ab9b5b11bae3e614c24c286d60250a36
changwoolab <cwyoo01@naver.com>
2024-06-18 10:49:55 +0900

    test: add more bench tests to ensure that es-toolkit is faster than lodash at most cases

--- benchmarks/intersection.bench.ts
+++ benchmarks/intersection.bench.ts
@@ -15,8 +15,8 @@ describe('intersection, small arrays', () => {
   });
 });
 
-describe('intersection, large arrays', () => {
-  const array1 = Array.from({ length: 10000 }, () => Math.floor(Math.random() * 1000));
+describe('intersection, large array with small numbers', () => {
+  const array1 = Array.from({ length: 1000 }, () => Math.floor(Math.random() * 1000));
   const array2 = Array.from({ length: 10000 }, () => Math.floor(Math.random() * 1000));
 
   bench('es-toolkit', () => {
@@ -27,3 +27,46 @@ describe('intersection, large arrays', () => {
     intersectionLodash(array1, array2);
   });
 });
+
+describe('intersection, large array with large numbers', () => {
+  const array1 = Array.from({ length: 10 }, () => Math.floor(Math.random() * 1000000000 + 100000));
+  const array2 = Array.from({ length: 5000 }, () => Math.floor(Math.random() * 1000000000 + 100000));
+
+  bench('es-toolkit', () => {
+    intersectionToolkit(array1, array2);
+  });
+
+  bench('lodash', () => {
+    intersectionLodash(array1, array2);
+  });
+});
+
+describe('intersection, large arrays with short strings', () => {
+  const array1 = Array.from({ length: 10000 }, () => getLongRandomString(10));
+  const array2 = Array.from({ length: 10000 }, () => getLongRandomString(10));
+
+  bench('es-toolkit', () => {
+    intersectionToolkit(array1, array2);
+  });
+
+  bench('lodash', () => {
+    intersectionLodash(array1, array2);
+  });
+});
+
+describe('intersection, large arrays with long strings', () => {
+  const array1 = Array.from({ length: 10 }, () => getLongRandomString(1000));
+  const array2 = Array.from({ length: 10000 }, () => getLongRandomString(1000));
+
+  bench('es-toolkit', () => {
+    intersectionToolkit(array1, array2);
+  });
+
+  bench('lodash', () => {
+    intersectionLodash(array1, array2);
+  });
+});
+
+function getLongRandomString(length: number) {
+  return Array.from({ length }, () => Math.floor(Math.random() * 1000).toString()).join('');
+}
